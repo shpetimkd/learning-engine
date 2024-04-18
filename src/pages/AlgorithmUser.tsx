344dec2a04dc063f88e10b22fdd1ee4d15d2f4f7
Shpetim <kadrijashpetim@gmail.com>
2024-04-18 15:37:09 +0200

    Generate dataset view

--- src/pages/AlgorithmUser.tsx
+++ src/pages/AlgorithmUser.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Card, Col, Row } from 'antd';
+import { Alert, Avatar, Button, Card, Col, InputNumber, Row } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
+import { valueType } from 'antd/es/statistic/utils';
+const { Meta } = Card;
 
 const AlgorithmsUser: React.FC = () => {
   const baseUrl = String(process.env.REACT_APP_BACKEND_URL);
+  const [alertMessage, setAlertMessage] = useState<string>('');
+  const [gamesPerConcept, setGamesPerConcept] = useState<valueType>(10);
+
   const onFinish = async (user: string) => {
     try {
       const response = await axios.post(`${baseUrl}generate-data`, {
         user: user,
-        games_per_concept: 5
+        games_per_concept: gamesPerConcept
       });
 
-      if (response.status === 201) {
+      if (response.status === 200) {
+        setAlertMessage('Dataset generated successfully');
 
         setTimeout(() => {
-          // setAlertMessage('');
+          setAlertMessage('');
         }, 3000);
       }
     } catch (error) {
@@ -24,28 +31,75 @@ const AlgorithmsUser: React.FC = () => {
 
   return (
     <div style={{ background: '', padding: 24, minHeight: '100vh' }}>
+      {
+        alertMessage && <Alert message={alertMessage} type="success" style={{ marginBottom: 12 }} />
+      }
       <Row gutter={16}>
         <Col span={8}>
-          <Card title="Billy" bordered={false}>
-            <Button type="primary" onClick={() => onFinish('user1')}>
-              Generate user data
-            </Button>
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://img.freepik.com/free-vector/computer-circuit-board-hand-shape-creative-technology-poster-vector-illustration_98292-591.jpg?t=st=1713445750~exp=1713449350~hmac=04485923e89cafcc40c005bf1aa337c89dac10c2693a139741abff1074f92011&w=1380"
+              />
+            }
+            actions={[
+              <><InputNumber style={{ width: '200px' }} key="number_input" placeholder='Games for concept' onChange={(val) => setGamesPerConcept(val || 10)} />
+                <SyncOutlined style={{ color: 'blue', marginTop: 20 }} key="setting" onClick={() => onFinish('user1')} /></>
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=12" />}
+              title="Mila"
+              description="Despite Mila's love for all things tech, she once spent an entire week convinced that cloud computing involved actual clouds — she's still waiting for sunny days to boost her Wi-Fi signal."
+            />
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="Bobby" bordered={false}>
-            <Button type="primary" onClick={() => onFinish('user2')}>
-              Generate user data
-            </Button>
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://img.freepik.com/free-vector/computer-circuit-board-hand-shape-creative-technology-poster-vector-illustration_98292-591.jpg?t=st=1713445750~exp=1713449350~hmac=04485923e89cafcc40c005bf1aa337c89dac10c2693a139741abff1074f92011&w=1380"
+              />
+            }
+            actions={[
+              <><InputNumber style={{ width: '200px' }} key="number_input" placeholder='Games for concept' onChange={(val) => setGamesPerConcept(val || 10)} />
+                <SyncOutlined style={{ color: 'blue', marginTop: 20 }} key="setting" onClick={() => onFinish('user2')} /></>
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+              title="Theo"
+              description="Theo has a mysterious talent for finding lost socks, leading his friends to believe he's actually a retired sock detective with a remarkable track record."
+            />
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="B" bordered={false}>
-            <Button type="primary" onClick={() => onFinish('user3')}>
-              Generate user data
-            </Button>
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://img.freepik.com/free-vector/computer-circuit-board-hand-shape-creative-technology-poster-vector-illustration_98292-591.jpg?t=st=1713445750~exp=1713449350~hmac=04485923e89cafcc40c005bf1aa337c89dac10c2693a139741abff1074f92011&w=1380"
+              />
+            }
+            actions={[
+              <><InputNumber style={{ width: '200px' }} key="number_input" placeholder='Games for concept' onChange={(val) => setGamesPerConcept(val || 10)} />
+                <SyncOutlined style={{ color: 'blue', marginTop: 20 }} key="setting" onClick={() => onFinish('user3')} /></>
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=9" />}
+              title="Ivy"
+              description="Ivy claims her green thumb is so powerful, she just has to look at a plant to make it grow — she's currently accepting challenges to revive your most hopeless houseplants."
+            />
           </Card>
         </Col>
+      </Row>
+      <Row>
       </Row>
     </div>
   );
--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -7,7 +7,16 @@ const { Title } = Typography;
 const { Text } = Typography;
 
 interface AlgorithmsResponse { name: string; id: string }
-interface LearnersCollection { name: string; days: number; path: any[]; results: any }
+interface LearnersCollection { name: string; days: number; path: any[]; results: any, algorithm: {
+  defaultPath: string;
+  sequence_id: string;
+  tutorial_less_than: number,
+  reinforcement_greater_than: number,
+  name: string;
+  id: string;
+  minimum_dataset_size: number,
+  enable_gates: boolean
+} }
 
 const defaultStyle = {
   backgroundColor: '#e5e5f7',
@@ -128,42 +137,56 @@ export default function UserDetails() {
         style={{ width: '100%' }}
       />
 
-      <List
-        dataSource={userData}
-        renderItem={item => (
-          <List.Item>
-            <Space align="start" size="large">
-              <Title level={5}>Results</Title>
-              <Flex gap="middle">
-                <Flex vertical={true}>
-                  {Object.entries(item.results).map((c: any) => {
-                    return (
-                      <Tag
-                        color={'blue'}
-                        key={c[0]}
-                        style={{
-                          display: 'inline-flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          overflow: 'hidden',
-                          fontSize: '0.75rem',
-                          whiteSpace: 'nowrap',
-                          textOverflow: 'ellipsis',
-                          color: 'black',
-                          margin: 4,
-                        }}
-                      >
-                        {c?.[0]}: Correct: {c?.[1]?.correct}, Incorrect: {c?.[1]?.incorrect}
-                      </Tag>
-                    );
-                  })}
-                </Flex>
-              </Flex>
-            </Space>
-          </List.Item>
-        )}
-        style={{ width: '100%' }}
-      />
+      <Row>
+        <Col span={7}>
+          <List
+            dataSource={userData}
+            renderItem={item => (
+              <List.Item>
+                <Space align="start" size="large">
+                  <Title level={5}>Results</Title>
+                  <Flex gap="middle">
+                    <Flex vertical={true}>
+                      {Object.entries(item.results).map((c: any) => {
+                        return (
+                          <Tag
+                            color={'blue'}
+                            key={c[0]}
+                            style={{
+                              display: 'inline-flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              overflow: 'hidden',
+                              fontSize: '0.75rem',
+                              whiteSpace: 'nowrap',
+                              textOverflow: 'ellipsis',
+                              color: 'black',
+                              margin: 4,
+                            }}
+                          >
+                            {c?.[0]}: Correct: {c?.[1]?.correct}, Incorrect: {c?.[1]?.incorrect}
+                          </Tag>
+                        );
+                      })}
+                    </Flex>
+                  </Flex>
+                </Space>
+              </List.Item>
+            )}
+            style={{ width: '100%' }}
+          />
+
+        </Col>
+        <Col span={12} style={{ margin: 12 }}>
+          <Text>
+            This configuration details the setup for <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.name}</Text>, a machine learning algorithm. It initializes using a specified <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.defaultPath}</Text>, ensuring consistency in data handling and processing workflows.
+            The algorithm is structured to process data sequences based on the <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.sequence_id}</Text> method, strategically prioritizing the least proficient data points to optimize learning and adaptation processes early in the training phase.
+            Key performance thresholds are set to dynamically adjust the learning interventions: a tutorial mode is triggered for predictions below a <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.tutorial_less_than}</Text>% accuracy threshold, while a reinforcement strategy is applied when prediction accuracy exceeds <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.reinforcement_greater_than}</Text>%. This approach allows for targeted learning enhancements based on model performance metrics.
+            A minimum dataset size of <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.minimum_dataset_size}</Text> is required, ensuring the algorithm has sufficient data to generate reliable predictions and avoid overfitting on sparse data.
+            Additionally, the <Text style={{ fontWeight: 600 }}>{userData?.[0]?.algorithm.enable_gates}</Text> flag is used to activate or deactivate conditional processing gates, providing flexibility in managing complex learning pathways or experimental features.
+          </Text>
+        </Col>
+      </Row>
     </div>
   );
 }
